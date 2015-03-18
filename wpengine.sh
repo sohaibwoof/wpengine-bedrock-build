5f265c0e2cf2e4e2573b1042a4c2cdabbe259461
Toby Schrapel <toby@tobyschrapel.com>
2015-03-18 15:09:54 +0000

    Remove uploads folder, force push

--- wpengine.sh
+++ wpengine.sh
@@ -22,18 +22,18 @@ git checkout -b wpengine
 cp -r web/app wp-content
 rm "wp-content/themes/${theme}/.gitignore"
 rm .gitignore
-echo "/*\n!wp-content/" >> .gitignore
+echo "/*\n!wp-content/\nwp-content/uploads" >> .gitignore
 git ls-files | xargs git rm --cached
 
 git add .
 git commit -am "Setting up WPEngine build."
 if [ "$environment" == "staging" ]
 then
-  git push staging wpengine:master
+  git push staging wpengine:master --force
   git checkout develop
 elif [ "$environment" == "production" ]
 then
-  git push production wpengine:master
+  git push production wpengine:master --force
   git checkout master
 fi
 git branch -D wpengine
