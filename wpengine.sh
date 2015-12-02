46b5c7dc2f16a2a8913903303b1c642ea810ac44
Toby Schrapel <toby@tobyschrapel.com>
2015-12-02 14:29:53 -0500

    Changes to deployment script to make the deployment process non-destuctive of the local development version. Thanks to @ollietreend

--- wpengine.sh
+++ wpengine.sh
@@ -1,59 +1,114 @@
+#!/usr/bin/env bash
+
 environment=$1
 theme="sage"
 
+###
+# Check that we have the correct working directory.
+###
+if [ ! -d "web" ]
+then
+  echo "Please run this script from the main htdocs directory."
+  exit
+fi
 
-echo "Starting the build process..."
+###
+# Stop if there are uncommitted changes
+###
+if [[ -n $(git status -s) ]]
+then
+  echo "Please review and commit your changes before continuing."
+  exit
+fi
+
+###
+# Create "deploy" directory
+# If necessary, remove existing directory
+###
+cd ".."
+if [ -d "deploy" ]
+then
+  echo "Removing old deployment directory."
+  rm -Rf "deploy"
+fi
+
+echo "Preparing files for deployment."
+cp -a "htdocs" "deploy"
+cd "deploy"
+
+###
+# Build theme assets with gulp
+###
+echo "Building theme assets."
+cd "web/app/themes/${theme}"
+npm install
+bower install
 if [ "$environment" == "staging" ]
 then
-  git checkout develop &> /dev/null
-  gulp &> /dev/null
+  git checkout develop
+  gulp
 elif [ "$environment" == "production" ]
 then
-  git checkout master &> /dev/null
-  gulp --production &> /dev/null
+  git checkout master
+  gulp --production
 else
   echo "Invalid environment."
   exit
 fi
+cd "../../../.."
 
-if [[ -n $(git status -s) ]]
-then
-  echo "Please review and commit your changes before continuing..."
-  exit
-fi
-
+###
+# Create a temporary wpengine branch
+###
 exists=`git show-ref refs/heads/wpengine`
 if [ -n "$exists" ]
 then
-  git branch -D wpengine &> /dev/null
+  git branch -D wpengine
 fi
-git checkout -b wpengine &> /dev/null
+git checkout -b wpengine
 
-cp -r web/app wp-content &> /dev/null
-rm "wp-content/themes/${theme}/.gitignore" &> /dev/null
-rm "wp-content/mu-plugins/bedrock-autoloader.php" &> /dev/null
-rm "wp-content/mu-plugins/disallow-indexing.php" &> /dev/null
-rm "wp-content/mu-plugins/register-theme-directory.php" &> /dev/null
-rm .gitignore &> /dev/null
+###
+# Move files into the expected locations.
+# Remove unwanted files.
+###
+mv web/app wp-content
+rm -R web
+rm "wp-content/themes/${theme}/.gitignore"
+rm "wp-content/mu-plugins/bedrock-autoloader.php"
+rm "wp-content/mu-plugins/disallow-indexing.php"
+rm "wp-content/mu-plugins/register-theme-directory.php"
+rm .gitignore
 echo "/*\n!wp-content/\nwp-content/uploads" >> .gitignore
-git ls-files | xargs git rm --cached &> /dev/null
+git ls-files | xargs git rm --cached
 
 cd wp-content/
 find . | grep .git | xargs rm -rf
 cd ../
 
-git add . &> /dev/null
-git commit -am "WPEngine build from: $(git log -1 HEAD --pretty=format:%s)$(git rev-parse --short HEAD 2> /dev/null | sed "s/\(.*\)/@\1/")" &> /dev/null
-echo "Pushing to WPEngine..."
+###
+# Commit new structure into git, and push to remote.
+###
+git add .
+git commit -am "WP Engine build from: $(git log -1 HEAD --pretty=format:%s)$(git rev-parse --short HEAD 2> /dev/null | sed "s/\(.*\)/@\1/")"
+
+echo "Pushing to WP Engine..."
 if [ "$environment" == "staging" ]
 then
-  git push staging wpengine:master --force &> /dev/null
-  git checkout develop &> /dev/null
+  git push staging wpengine:master --force
+  git checkout develop
 elif [ "$environment" == "production" ]
 then
-  git push production wpengine:master --force &> /dev/null
-  git checkout master &> /dev/null
+  git push production wpengine:master --force
+  git checkout master
 fi
-git branch -D wpengine &> /dev/null
-rm -rf wp-content/ &> /dev/null
+git branch -D wpengine
 echo "Successfully deployed."
+
+###
+# Remove deploy directory and move back to htdocs
+###
+echo "Cleaning up..."
+cd "../htdocs"
+rm -Rf "../deploy"
+git fetch
+echo "Done."
